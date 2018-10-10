73538fee902ecec5a2565184c9d81113cf39ff4c
Clay_wang <Clay_wang@vip.163.com>
2018-10-11 07:13:25 +0800

    留言板css完成，修改topNav博客跳转bug

--- js/message.js
+++ js/message.js
@@ -1,75 +1,79 @@
-!function() {
-    var view = document.querySelector('section.message')
+!function () {
+  var view = document.querySelector('section.message')
 
-    var model = {
-        //获取数据
-        init:function () {
-            var APP_ID = 'YRnz8E4vVkPA4U29WBOoFxmr-gzGzoHsz';
-            var APP_KEY = 's9yycLcF9E3QLWBvxJfeWazR';
-            AV.init({appId: APP_ID, appKey: APP_KEY});
-        },
-        fetch:function () {
-            var query = new AV.Query('Message');
-            return query.find() //promise对象
-        },
-        //创建数据
-        save:function (name,content) {
-            var Message = AV.Object.extend('Message');
-            var message = new Message();
-            return message.save({ //promise对象
-                'name': name,
-                'content': content
-            })
-        }
+  var model = {
+    //获取数据
+    init: function () {
+      var APP_ID = 'YRnz8E4vVkPA4U29WBOoFxmr-gzGzoHsz';
+      var APP_KEY = 's9yycLcF9E3QLWBvxJfeWazR';
+      AV.init({appId: APP_ID, appKey: APP_KEY});
+    },
+    fetch: function () {
+      var query = new AV.Query('Message');
+      return query.find() //promise对象
+    },
+    //创建数据
+    save: function (name, content) {
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      return message.save({ //promise对象
+        'name': name,
+        'content': content
+      })
     }
+  }
 
-    var controller = {
-        view: null,
-        model:null,
-        messageList:null,
-        init:function (view) {
-            this.view = view
-            this.model = model
-            this.messageList = view.querySelector('#messageList')
-            this.form = view.querySelector('form')
-            this.model.init()
-            this.loadMessages()
-            this.bindEvents()
-        },
-        loadMessages:function () {
-            this.model.fetch().then(
-                (messages) => {
-                    let array = messages.map((item)=> item.attributes )
-                    array.forEach((item)=> {
-                        let li = document.createElement('li')
-                        li.innerText = `${item.name}: ${item.content}`
-                        this.messageList.appendChild(li)
-                    })
-                })
-        },
-        bindEvents:function () {
-            this.form.addEventListener('submit',(e)=> {
-                e.preventDefault()
-                this.saveMessage()
-            })
-        },
-        saveMessage:function () {
-            let myForm = this.form
-            let content = myForm.querySelector('input[name=content]').value
-            let name = myForm.querySelector('input[name=name]').value
-            this.model.save(name,content).then(function(object) {
-                let li = document.createElement('li')
-                li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-                let messageList = document.querySelector('#messageList')
-                messageList.appendChild(li)
-                myForm.querySelector('input[name=content]').value = ''
-                myForm.querySelector('input[name=name]').value = ''
-            })
-        },
-
-    }
-
-    controller.init(view)
-
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view) {
+      this.view = view
+      this.model = model
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('input[name=submitButton]')
+      console.log(this.form)
+      this.inputContent = view.querySelector('#inputContent')
+      this.inputUserName = view.querySelector('#inputUserName')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function () {
+      this.model.fetch().then(
+        (messages) => {
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
+            let li = document.createElement('li')
+            li.innerText = `${item.name}: ${item.content}`
+            this.messageList.appendChild(li)
+          })
+        })
+    },
+    bindEvents: function () {
+      this.form.addEventListener('submit', (e) => {
+        console.log(1)
+        e.preventDefault()
+        console.log(3)
+        this.saveMessage()
+        console.log(2)
+      })
+    },
+    saveMessage: function () {
+      let divInput = this.inputContent
+      let divUserName = this.inputUserName
+      let content = divInput.querySelector('input[name=content]').value
+      let name = divUserName.querySelector('input[name=name]').value
+      this.model.save(name, content).then(function (object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        divInput.querySelector('input[name=content]').value = ''
+        divUserName.querySelector('input[name=name]').value = ''
+      })
+    },
+  }
+  controller.init(view)
 }.call()
 
